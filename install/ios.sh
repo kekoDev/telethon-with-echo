a814a21146c817ccc6accebfd8e9c29d8f6a43f9
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-03 22:22:06 +0800

    Update ios.sh

--- install/ios.sh
+++ install/ios.sh
@@ -3,5 +3,6 @@ apk get python3
 apk get py-pip
 pip install telethon
 pip install python-telegram-bot
+wget "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot.py"
 reset 
 python3 bot.py
