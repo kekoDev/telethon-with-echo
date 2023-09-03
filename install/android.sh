83995908811243a0f321dcc61a91f6e5c6a7e5fc
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-03 22:24:17 +0800

    Update android.sh

--- install/android.sh
+++ install/android.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+pkg update
+pkg install python3 -y
+pip3 install telethon
+pip3 install requests
+pip3 install python-telegram-bot
+wget "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot.py"
+reset 
+python3 bot.py
