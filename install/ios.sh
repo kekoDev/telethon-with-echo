c8abb1b0d45407dda0d7081f8802cbdd1d299726
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-04 14:44:01 +0800

    Update ios-run.sh

--- install/ios.sh
+++ install/ios.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
-apk add python3
-apk add py-pip
-pip install python-telegram-bot telethon
-wget "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot.py"
-reset 
-python3 bot.py 
 
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed."
+    apk add python3
+fi
+
+# Check if pip is installed
+if ! command -v pip &> /dev/null; then
+    echo "pip is not installed."
+    apk add py-pip
+fi
+
+# Install Python packages
+pip install python-telegram-bot telethon
+
+# Check if the installation was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to install Python packages."
+    exit 1
+fi
+
+# Create a temporary file to store the downloaded script
+temp_script=$(mktemp)
+
+# Download the Python script using wget
+if ! wget -q "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" -O "$temp_script"; then
+    echo "Failed to download the Python script."
+    exit 1
+fi
+
+# Run the downloaded Python script
+if ! python3 "$temp_script"; then
+    echo "Failed to run the Python script."
+    exit 1
+fi
+
+# Clean up the temporary file
+rm "$temp_script"
