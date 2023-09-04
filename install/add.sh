95d7d01a48f924dbb6ce61940aef3cd961188c71
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-04 15:15:31 +0800

    Update and rename ad.sh to add.sh

--- install/add.sh
+++ install/add.sh
@@ -4,6 +4,7 @@
 if ! command -v python3 &> /dev/null; then
     echo "Python 3 is not installed."
     termux-change-repo
+    pkg update
     pkg install python3
     pip3 install telethon requests python-telegram-bot
 fi
