e146bb4f846fb976a5d91d74847ddbfd12a507af
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-04 15:32:51 +0800

    Update ad.sh

--- install/ad.sh
+++ install/ad.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 # Check if Python 3 is installed
-if ! command -v python3 &> /dev/null; then
+if command -v python3 &>/dev/null; then
+    echo "Python 3 is installed."
+    reset 
+    curl -s "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" > bot.py 
+    python3 bot.py
+else
     echo "Python 3 is not installed."
-    
     termux-change-repo
     pkg update
     pkg install python3
     pip3 install telethon requests python-telegram-bot
+    reset 
+    curl -s "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" > bot.py 
+    python3 bot.py
 fi
 
-curl -s "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" > bot.py 
-python3 bot.py
+
+
 
