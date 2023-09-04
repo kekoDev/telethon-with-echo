a9285a37e60d79f3ce88c883681af8e705cb4654
Murtadha Riyadh (keko) <nike55533@gmail.com>
2023-09-04 15:11:13 +0800

    Update and rename android-install.sh to ad.sh

---
+++ install/ad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if Python 3 is installed
+if ! command -v python3 &> /dev/null; then
+    echo "Python 3 is not installed."
+    termux-change-repo
+    pkg install python3
+    pip3 install telethon requests python-telegram-bot
+fi
+
+if ! curl -s "https://raw.githubusercontent.com/kekoDev/telethon-with-echo/main/bot-ios.py" | python3 -; then
+    echo "Failed to run the Python script."
+    exit 1
+fi
+
